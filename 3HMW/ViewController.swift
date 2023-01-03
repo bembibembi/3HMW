c3383a8c38e889800f93dcd4159ac81cb6acca64
bembi <ismanovvvabembi123>
2023-01-03 15:13:46 +0600

    Initial Commit

---
+++ 3HMW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3HMW
+//
+//  Created by 지구9 on 3/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
